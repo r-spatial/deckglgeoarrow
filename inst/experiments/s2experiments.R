e848ad8738624934eba60c36d819fa8a48887a5c
Tim Appelhans <tim.appelhans@addium.io>
2026-07-13 17:27:39 +0200

    add some experiments for s2 layer implementation

---
+++ inst/experiments/s2experiments.R
@@ -0,0 +1,17 @@
+library(wk)
+library(s2)
+
+n = 1e6
+
+pts = data.frame(
+  id = seq_len(n)
+  , geometry = xy(
+    x = runif(n, -180, 180)
+    , y = runif(n, -70, 70)
+    , crs = 4326
+  )
+)
+
+pts$s2cell = as_s2_cell(pts$geometry)
+pts$s2parent_l4 = s2_cell_parent(pts$s2cell, level = 4)
+
